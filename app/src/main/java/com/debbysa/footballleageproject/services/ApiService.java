4f29b9175e8206dad77689c524687794be046e5f
debbysa <deby.silvia23@gmail.com>
2019-05-25 16:09:44 +0700

    menambahkan service

--- app/src/main/java/com/debbysa/footballleageproject/services/ApiService.java
+++ app/src/main/java/com/debbysa/footballleageproject/services/ApiService.java
@@ -1,4 +1,25 @@
 package com.debbysa.footballleageproject.services;
 
+import com.debbysa.footballleageproject.generator.ServiceGenerator;
+import com.debbysa.footballleageproject.model.Competitions;
+import com.debbysa.footballleageproject.model.Matches;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+
 public interface ApiService {
+    @Headers(ServiceGenerator.BASE_KEY)
+    @GET("/v2/competitions")
+    Call<Competitions> getCompetitions();
+
+    @Headers(ServiceGenerator.BASE_KEY)
+    @GET("/v2/matches")
+    Call<Matches> getFixtures();
+
+    @Headers(ServiceGenerator.BASE_KEY)
+    @GET("/v2/competitions/{id}/matches")
+    Call<Matches> getMatches(@Path("id") int id);
+
 }

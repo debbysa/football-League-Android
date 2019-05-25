15d83faed44aa13ea9f55bbe248f96d6eca4dd74
debbysa <deby.silvia23@gmail.com>
2019-05-25 16:11:01 +0700

    memperbaruhi model

--- app/src/main/java/com/debbysa/footballleageproject/model/Matches.java
+++ app/src/main/java/com/debbysa/footballleageproject/model/Matches.java
@@ -1,4 +1,27 @@
 package com.debbysa.footballleageproject.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Matches {
+    // for today's matchList (matches) list
+    // https://api.football-data.org/v2/matchList
+
+    @SerializedName("count")
+    @Expose
+    private Integer countMatches;
+
+    @SerializedName("matches")
+    @Expose
+    private List<Match> matchList;
+
+    public Integer getCountMatches() {
+        return countMatches;
+    }
+
+    public List<Match> getMatchList() {
+        return matchList;
+    }
 }
